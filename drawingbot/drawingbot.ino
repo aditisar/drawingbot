0f2ae09b0afdb760e488e8450df7e4ac79bbba06
aditisar <aditi0821@gmail.com>
2014-11-06 18:22:50 -0500

    newer version with sensor

--- drawingbot/drawingbot.ino
+++ drawingbot/drawingbot.ino
@@ -1,19 +1,44 @@
 int MB1 = 6; // motor b pin 1
 int MB2 = 5; // motor b pin 2
+int sensor1 = 2;  // change "0" to whatever pin to which you are hooking the sensor. Just this one change allows you to test operation on other pins.
+int reflectance;
 void setup() {
   pinMode(MB1, OUTPUT);
   pinMode(MB2, OUTPUT);
 }
 void loop() {
-  doForward(MB1, MB2); // motor B forward
-  delay(200);
-  doStop(MB1, MB2);
-  delay(400);
+    reflectance = 1;                   //initialize value to 1 at the beginning of each loop
+  pinMode(sensor1, OUTPUT);   //set pin as output
+  digitalWrite(sensor1, HIGH);  //set pin HIGH (5V)
+  delayMicroseconds(15);        //charge capacitor for 15 microseconds
+   
+  pinMode(sensor1, INPUT);                                          //set pin as input
+     while((reflectance < 900) && (digitalRead(sensor1) != LOW)){   //timeout at 500
+                                                                    // read the pin state, increment counter until state = LOW
+          ++ reflectance;                                           // increment value to be displayed via serial port
+            //  delayMicroseconds(4);                                 //Change value or comment out to adjust value range
+      }
   
-  doBackward(MB1, MB2); //motor B backward
-  delay(200);
+  if (reflectance < 500){
+      Serial.println(reflectance);}   //Send reflectance value to serial display
+      else {
+        Serial.println("T.O.");       //if reflectance value is over 500 then it's a "timeout"
+      }
+       
+  delay(0);
+  Serial.begin(9600);
+  doForward(MB1, MB2); // motor B forward
+  delay(0);
   doStop(MB1, MB2);
-  delay(400);
+  delay(0);
+  
+  if (reflectance > 200) {
+  doBackward(MB1, MB2); //motor B backward
+  delay(10);
+  doStop(MB1, MB2);
+  delay(0);
+  
+  }
 }
 void doForward(int pin1, int pin2) {
   digitalWrite(pin2, LOW);
